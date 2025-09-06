4ee50500eaecc10492b300c7f87223328a2be4ca
Vishesh Vishwakarma <61264209+Vishesh270@users.noreply.github.com>
2025-09-06 23:19:36 +0530

    Add files via upload

---
+++ src/main/java/com/jenkins_build/spring_jenkins/SpringJenkinsApplication.java
@@ -0,0 +1,17 @@
+package com.jenkins_build.spring_jenkins;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+		System.out.println("Hi i am from project ");
+
+	}
+
+}
