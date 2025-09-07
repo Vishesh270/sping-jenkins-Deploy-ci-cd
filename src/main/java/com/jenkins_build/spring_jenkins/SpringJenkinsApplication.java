7c0241a7b5abd32fae478faa895a3df21e41a065
Vishesh <visheshvishwakarma2700@gmail.com>
2025-09-07 15:04:24 +0530

    test commit

--- src/main/java/com/jenkins_build/spring_jenkins/SpringJenkinsApplication.java
+++ src/main/java/com/jenkins_build/spring_jenkins/SpringJenkinsApplication.java
@@ -10,7 +10,7 @@ public class SpringJenkinsApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringJenkinsApplication.class, args);
-		System.out.println("Hi i am from project ");
+		System.out.println("Hi i am from project  ");
 
 	}
 
